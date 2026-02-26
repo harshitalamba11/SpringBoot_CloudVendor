4b946d607d0fbbe72e2b68b696a7bdaed516282b
harshitalamba11 <lambaharshita1@gmail.com>
2026-02-27 00:22:10 +0530

    CORS Updated

--- src/main/java/com/morgan/rest_demo/config/CorsConfig.java
+++ src/main/java/com/morgan/rest_demo/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedMethods("*")   // <-- allow ALL methods
+                        .allowedHeaders("*");  // <-- allow ALL headers
             }
         };
     }
